ee53f63a42605e65340e253ec99d4d2cc3714076
NirpendraNathMishra <NirpendraNathMishra1947@gmail.com>
2024-02-15 07:40:23 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Video from './components/video.jsx';
+import VideoPlayer from './components/videoplayer.jsx';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from "./components/store.js";
+
+function App() {
+    return (
+        <Provider store={store}>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Video />} />
+                    <Route path="/video/:id" element={<VideoPlayer />} />
+                </Routes>
+            </Router>
+        </Provider>
+    );
+}
+
+export default App;
